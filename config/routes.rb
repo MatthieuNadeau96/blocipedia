13dafdff7f5b7f8bbd9a865e594bc5bab6f5baaf
Matthieu <matthieunadeau96@gmail.com>
2017-05-26 16:37:13 +0000

    Checkpoint 2: User Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,28 @@
 Rails.application.routes.draw do
+  #get 'welcome/index'
+
+  # devise_for :models
+  devise_for :users
+  
+# Rails.application.routes.draw do
+
+  #get 'welcome/index'
+
+#   resources :collaborators#, only: [:create, :destroy]
+#   resources :wikis
+#   root 'welcome#index'
+#   get 'about' => 'welcome#about'
+#   devise_for :users
+#   resources :charges, only: [:new, :create]
+#   get 'downgrade' => 'charges#downgrade'
+# end
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
